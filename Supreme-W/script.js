1fa96e0450f2d84758a8e2005715777af603ef7b
Dominique Rejouis <97000744+Dommperry@users.noreply.github.com>
2023-01-22 18:17:43 -0800

    Add files via upload

---
+++ Supreme-W/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let time = document.getElementById("current-time");
+let date = document.getElementById("current-date");
+
+setInterval(() => {
+  let d = new Date();
+  time.innerHTML = d.toLocaleTimeString();
+  date.innerHTML = d.toLocaleDateString();
+}, 1000);
+
+
